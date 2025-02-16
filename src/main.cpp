ebc934fbb6ce449dfc45422af7bd1604d9082744
Henry LETELLIER <henry.letellier@epitech.eu>
2025-02-16 16:58:24 +0100

    [UPDATE] the method used to attempt to allow calls to be dual way instead of single way

--- src/main.cpp
+++ src/main.cpp
@@ -50,71 +50,69 @@ int main(int argc, char **argv)
     bool debug = defaultDebug;
     unsigned int maxRounds = DefaultLoopLimit;
     // Check the arguments if present
-    if (argc > 0) {
-        for (int i = 1; i < argc; i++) {
-            std::string arg = std::string(argv[i]);
-            if (arg == "-p") {
-                if (i + 1 < argc) {
-                    port = std::stoi(argv[i + 1]);
-                    i++;
-                } else {
-                    std::cout << "Missing argument parameter, use -h for help" << std::endl;
-                }
-            } else if (arg == "-i") {
-                if (i + 1 < argc) {
-                    ip = argv[i + 1];
-                    i++;
-                } else {
-                    std::cout << "Missing argument parameter, use -h for help" << std::endl;
-                }
-            } else if (arg == "-r") {
-                is_sender = false;
-            } else if (arg == "-s") {
-                is_sender = true;
-            } else if (arg == "-d") {
-                debug = true;
-            } else if (arg == "-l") {
-                log = true;
-            } else if (arg == "-m") {
-                if (i + 1 < argc) {
-                    maxRounds = std::stoi(argv[i + 1]);
-                    i++;
-                } else {
-                    std::cout << "Missing argument parameter, use -h for help" << std::endl;
-                }
-            } else if (arg == "-e") {
-                myCapsule.setEcho(true);
-            } else if (arg == "-a") {
-                BootScreen.displayAllScreens();
-                return PROGRAM_SUCCESS;
-            } else if (arg == "-h" || arg == "--help") {
-                std::cout << "USAGE:\n";
-                std::cout << std::string(argv[0]) << " -p <port> -i <ip> [-r <receiver> | -s <sender>] -d -l\n";
-                std::cout << "\n";
-                std::cout << "OPTIONS:\n";
-                std::cout << "-p <port> : Set the port to connect to (default: " << defaultPort << ")\n";
-                std::cout << "-i <ip> : Set the ip to connect to (default: " << defaultIp << ")\n";
-                std::cout << "-r <receiver> : Set the client to receiver mode (default: " << Recoded::myToString(!defaultSenderMode) << "\n";
-                std::cout << "-s <sender> : Set the client to sender mode (default: " << Recoded::myToString(defaultSenderMode) << ")\n";
-                std::cout << "-d : Enable debug mode (default: " << Recoded::myToString(defaultDebug) << ")\n";
-                std::cout << "-l : Enable log mode (default: " << Recoded::myToString(defaultLog) << ")\n";
-                std::cout << "-m <maxRounds> : Set the maximum number of rounds, 0 = endless (default: " << DefaultLoopLimit << ")\n";
-                std::cout << "-e : Enable echo mode for the user prompt (default: false)\n";
-                std::cout << "-a : Display all boot screens (Epilepsy warning, all the" << Recoded::myToString(BootScreen.getAvailableScreens()) << "logos will be displayed one after the other without any delay, meaning they will come out as fast as you terminal can diplay them)\n";
-                std::cout << "\n";
-                std::cout << "VERSION:\n";
-                std::cout << "The program's version is: " << VERSION << std::endl;
-                std::cout << "\n";
-                std::cout << "AUTHOR:\n";
-                std::cout << "Written by:";
-                std::cout << "\t-\tHenry Letellier              : https://github.com/HenraL\n";
-                std::cout << "Epitech student 2025\n";
-                std::cout << "\n" << std::flush;
-                return PROGRAM_SUCCESS;
+    for (int i = 1; i < argc; i++) {
+        std::string arg = std::string(argv[i]);
+        if (arg == "-p") {
+            if (i + 1 < argc) {
+                port = std::stoi(argv[i + 1]);
+                i++;
             } else {
-                std::cout << "Invalid argument, use -h for help" << std::endl;
-                std::cout << "The argument you provided was: " << argv[i] << std::endl;
+                std::cout << "Missing argument parameter, use -h for help" << std::endl;
             }
+        } else if (arg == "-i") {
+            if (i + 1 < argc) {
+                ip = argv[i + 1];
+                i++;
+            } else {
+                std::cout << "Missing argument parameter, use -h for help" << std::endl;
+            }
+        } else if (arg == "-r") {
+            is_sender = false;
+        } else if (arg == "-s") {
+            is_sender = true;
+        } else if (arg == "-d") {
+            debug = true;
+        } else if (arg == "-l") {
+            log = true;
+        } else if (arg == "-m") {
+            if (i + 1 < argc) {
+                maxRounds = std::stoi(argv[i + 1]);
+                i++;
+            } else {
+                std::cout << "Missing argument parameter, use -h for help" << std::endl;
+            }
+        } else if (arg == "-e") {
+            myCapsule.setEcho(true);
+        } else if (arg == "-a") {
+            BootScreen.displayAllScreens();
+            return PROGRAM_SUCCESS;
+        } else if (arg == "-h" || arg == "--help") {
+            std::cout << "USAGE:\n";
+            std::cout << std::string(argv[0]) << " -p <port> -i <ip> [-r <receiver> | -s <sender>] -d -l\n";
+            std::cout << "\n";
+            std::cout << "OPTIONS:\n";
+            std::cout << "-p <port> : Set the port to connect to (default: " << defaultPort << ")\n";
+            std::cout << "-i <ip> : Set the ip to connect to (default: " << defaultIp << ")\n";
+            std::cout << "-r <receiver> : Set the client to receiver mode (default: " << Recoded::myToString(!defaultSenderMode) << "\n";
+            std::cout << "-s <sender> : Set the client to sender mode (default: " << Recoded::myToString(defaultSenderMode) << ")\n";
+            std::cout << "-d : Enable debug mode (default: " << Recoded::myToString(defaultDebug) << ")\n";
+            std::cout << "-l : Enable log mode (default: " << Recoded::myToString(defaultLog) << ")\n";
+            std::cout << "-m <maxRounds> : Set the maximum number of rounds, 0 = endless (default: " << DefaultLoopLimit << ")\n";
+            std::cout << "-e : Enable echo mode for the user prompt (default: false)\n";
+            std::cout << "-a : Display all boot screens (Epilepsy warning, all the" << Recoded::myToString(BootScreen.getAvailableScreens()) << "logos will be displayed one after the other without any delay, meaning they will come out as fast as you terminal can diplay them)\n";
+            std::cout << "\n";
+            std::cout << "VERSION:\n";
+            std::cout << "The program's version is: " << VERSION << std::endl;
+            std::cout << "\n";
+            std::cout << "AUTHOR:\n";
+            std::cout << "Written by:";
+            std::cout << "\t-\tHenry Letellier              : https://github.com/HenraL\n";
+            std::cout << "Epitech student 2025\n";
+            std::cout << "\n" << std::flush;
+            return PROGRAM_SUCCESS;
+        } else {
+            std::cout << "Invalid argument, use -h for help" << std::endl;
+            std::cout << "The argument you provided was: " << argv[i] << std::endl;
         }
     }
 
@@ -140,12 +138,12 @@ int main(int argc, char **argv)
         Compressor::Manager myManager;
         Network::UDP myUDP(io_context, ip, port, is_sender);
         BootScreen.display();
-        if (is_sender) {
-            audio.record();
-            myCapsule.startThread();
-        } else {
-            audio.play();
-        }
+        // if (is_sender) {
+        audio.record();
+        myCapsule.startThread();
+        // } else {
+        audio.play();
+        // }
 
         while ((rounds < maxRounds || maxRounds == 0) && continueRunning == true && myUDP.isConnectionAlive()) {
             if (is_sender) {
@@ -189,13 +187,13 @@ int main(int argc, char **argv)
             PRETTY_INFO << "Sound buffer and compressed buffer cleared" << std::endl;
             rounds++;
         }
-        if (is_sender) {
-            audio.stopRecord();
-            PRETTY_INFO << "Sending end message" << std::endl;
-            myUDP.sendRaw(endMessage.c_str(), endMessage.size(), ip, port);
-        } else {
-            audio.stopPlay();
-        }
+        // if (is_sender) {
+        audio.stopRecord();
+        PRETTY_INFO << "Sending end message" << std::endl;
+        myUDP.sendRaw(endMessage.c_str(), endMessage.size(), ip, port);
+        // } else {
+        audio.stopPlay();
+        // }
         if (myCapsule.isRunning()) {
             try {
                 myCapsule.stopThread();
@@ -204,7 +202,7 @@ int main(int argc, char **argv)
                 PRETTY_WARNING << e.what() << std::endl;
             }
             catch (...) {
-                PRETTY_ERROR << "The thread stopping did not heppen in a clean manner" << std::endl;
+                PRETTY_ERROR << "The thread stopping did not happen in a clean manner" << std::endl;
             }
         }
         PRETTY_SUCCESS << "Program ended successfully" << std::endl;
